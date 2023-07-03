b4e679e90462b2cd87a4404fb576e82077a28f5e
iamsaddamhossen <saddamhossen.cse007@gmail.com>
2023-07-03 13:57:52 +0600

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 import './style.css';
 
 // Write Javascript code!
+
+console.log('Hello MERN');
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Learn MERN Stack</h1>`;
